41105660843363a11f16d90ab4e22e9eab9c121a
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-09 21:06:33 +1100

    initial setup

---
+++ data.js
@@ -0,0 +1,8 @@
+const SETTINGS = {
+    key: "ad6d47fddad6f9f566e190d9bbd550b9",
+    currentWeatherBaseURL: "https://api.openweathermap.org/data/2.5/weather?",
+    forecastBaseURL: "https://api.openweathermap.org/data/2.5/forecast?"
+};
+
+
+// https://api.openweathermap.org/data/2.5/find?appid=ad6d47fddad6f9f566e190d9bbd550b9&q=singapore
