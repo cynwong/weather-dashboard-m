d367a054c74eee0bcf8e0c775641fbceceb701e8
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-10 19:59:46 +1100

    change name

--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,5 @@
 const SETTINGS = {
-    key: "ad6d47fddad6f9f566e190d9bbd550b9",
+    apiKey: "ad6d47fddad6f9f566e190d9bbd550b9",
     currentWeatherBaseURL: "https://api.openweathermap.org/data/2.5/weather?",
     forecastBaseURL: "https://api.openweathermap.org/data/2.5/forecast?"
 };
