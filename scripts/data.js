aa26a2b8edf41262b8e215a71b9570267ad4e7d3
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-11 15:24:17 +1100

    add weather data

--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,38 @@
 const SETTINGS = {
     apiKey: "ad6d47fddad6f9f566e190d9bbd550b9",
-    currentWeatherBaseURL: "https://api.openweathermap.org/data/2.5/weather?",
-    forecastBaseURL: "https://api.openweathermap.org/data/2.5/forecast?"
+    // currentWeatherBaseURL: "https://api.openweathermap.org/data/2.5/weather?",
+    // forecastBaseURL: "https://api.openweathermap.org/data/2.5/forecast?",
+    baseURL: "https://api.openweathermap.org/data/2.5/",
+    nowWeatherLocation: "weather",
+    forecastLocation: "forecast",
+    uvIndexLocation: "uvi",
+
+    temperatureUnit: {
+            fullName: "Fahrenheit",
+            htmlSymbol: "&#x2109;",
+            api: "imperial"
+    },
+    humidity:{
+        unit: "%"
+    },
+    windSpeed:{
+        unit:"MPH"
+    },
+    uvIndex:{
+        unit: "", 
+        range: {
+            low: {min:0,max:2},
+            moderate: {min:3, max:5},
+            high: {min:6, max:7},
+            veryHigh:{min:8, max:10},
+            extreme: {min:11}
+        }
+    },
+    weatherIcon: {
+        url: "http://openweathermap.org/img/wn/",
+        suffixNormal : ".png",
+        suffixLarger: "@2x.png"
+    }
 };
 
 
