653e31d7cec2100e9b8460cda15e5c51c3575e70
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-11-11 23:38:48 +1100

    rename settings to api_settings

--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,4 @@
-const SETTINGS = {
+const API_SETTINGS = {
     apiKey: "ad6d47fddad6f9f566e190d9bbd550b9",
     // currentWeatherBaseURL: "https://api.openweathermap.org/data/2.5/weather?",
     // forecastBaseURL: "https://api.openweathermap.org/data/2.5/forecast?",
